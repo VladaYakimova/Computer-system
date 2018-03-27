8de7633c9fb55c777b6f6b0715bee9505115c84f
Vlada Yakimova <ladayakimova@i.ua>
2018-03-27 11:01:03 +0300

    Lab1

---
+++ Lab1/SizeOfFileAndEnthropy/SizeOfFileAndEnthropy/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<char, double> frequencies = new Dictionary<char, double>();
+
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Wrong input parameters");
+                return;
+            }
+            GetFrequencies(frequencies, args[0]);
+            double enth = Enthropy(frequencies);
+            double infoAmount = enth * frequencies.Keys.Count;
+
+
+
+
+            PrintFrequencies(frequencies);
+            Console.WriteLine("Enthropy: " + enth + "\n");
+            Console.WriteLine("Info amount: " + infoAmount + "\n");
+            Console.WriteLine("File size: " + new FileInfo(args[0]).Length);
+        }
+        static void GetFrequencies(Dictionary<char, double> frequencies, string pathToFile)
+        {
+            int length = 0;
+            using (StreamReader sr = new StreamReader(new FileStream(pathToFile, FileMode.Open)))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    length += line.Length;
+                    for (int i = 0; i < line.Length; ++i)
+                    {
+                        double val;
+                        if (frequencies.TryGetValue(line[i], out val))
+                        {
+                            frequencies[line[i]] = val + 1.0;
+                        }
+                        else
+                        {
+                            frequencies.Add(line[i], 1.0);
+                        }
+                    }
+                }
+            }
+            List<char> keys = new List<char>(frequencies.Keys);
+            for (int i = 0; i < keys.Count; ++i)
+            {
+                frequencies[keys[i]] /= length;
+            }
+        }
+
+        static double Enthropy(Dictionary<char, double> frequencies)
+        {
+            double enthropy = 0.0;
+            foreach (char key in frequencies.Keys)
+            {
+                enthropy += frequencies[key] * Math.Log(frequencies[key], 2.0);
+            }
+            return -enthropy;
+        }
+
+        static void PrintFrequencies(Dictionary<char, double> frequencies)
+        {
+            foreach (char key in frequencies.Keys)
+            {
+                Console.WriteLine(key + " : " + frequencies[key]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Lab1/base64/base64/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Base64Encode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Неправильний синтаксис. \n Правильно ------ Base64Encode.exe fileOut fileIn");
+                return;
+            }
+            Base64(args[0], args[1]);
+        }
+        static void Base64(string fileOut, string fileIn)
+        {
+            const string base64chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+            List<byte> chars = new List<byte>();
+            int c;
+            int tail = 0;
+            using (BinaryReader br = new BinaryReader(new FileStream(fileOut, FileMode.Open)))
+            {
+                chars = new List<byte>(br.ReadBytes((int)br.BaseStream.Length));
+            }
+            c = chars.Count % 3;
+            if (c > 0)
+            {
+                for (; c < 3; c++)
+                {
+                    chars.Add(0);
+                    ++tail;
+                }
+            }
+
+            string res = "";
+
+            for (int i = 0; i < chars.Count; i += 3)
+            {
+                res += base64chars[chars[i] >> 2];
+
+                int b2 = (chars[i] & 3) << 4;
+                b2 |= chars[i + 1] >> 4;
+                res += base64chars[b2];
+
+                int b3 = (chars[i + 1] & 15) << 2;
+                b3 |= chars[i + 2] >> 6;
+                res += base64chars[b3];
+
+                res += base64chars[chars[i + 2] & 63];
+            }
+
+            res = res.Substring(0, res.Length - tail);
+
+            for (int i = 0; i < tail; ++i)
+                res += '=';
+
+            using (StreamWriter sw = new StreamWriter(new FileStream(fileIn, FileMode.Create)))
+            {
+                sw.Write(res);
+            }
+        }
+    }
+}
+
